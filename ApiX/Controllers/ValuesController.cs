50906610b0782cb548e9e26a1646121a2142e559
Thiago Passos <thiago@passos.com.au>
2017-10-22 00:18:58 +1000

    adding projects

---
+++ ApiX/Controllers/ValuesController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiX.Controllers
+{
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] {
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "Nunc maximus iaculis lorem ut tempor.",
+                "Nam malesuada dui at viverra lobortis.",
+                "Donec ornare, sapien vitae ornare scelerisque, urna risus porta ante, et vulputate tortor diam sit amet magna.",
+                "Nullam purus purus, faucibus ut suscipit eget, bibendum vel ipsum.",
+                "Vestibulum risus est, rhoncus sed egestas nec, vulputate ut ligula.",
+                "Nam a pellentesque dolor, id condimentum lectus.",
+                "In at accumsan ante.",
+                "Praesent ut sagittis tortor, non gravida eros.",
+                "Curabitur dictum lacinia molestie.",
+                "Nullam ac mattis purus, et scelerisque nibh.",
+                "Donec lacinia, est vitae feugiat tincidunt, nisl quam tristique tellus, quis condimentum eros erat nec augue."
+            };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private HttpClient ApiXClient
+        {
+            get
+            {
+                var client = new HttpClient { BaseAddress = new Uri("apix") };
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                return client;
+            }
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var response = await ApiXClient.GetAsync("/");
+            if (response.IsSuccessStatusCode)
+            {
+                View(new { Code = response.StatusCode, Content = await response.Content.ReadAsStringAsync() });
+            }
+
+            return View(new { Code = response.StatusCode });
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
