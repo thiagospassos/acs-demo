114ad8d4286799586a16366c21672637d8613db5
Thiago Passos <thiago@passos.com.au>
2017-10-22 15:37:10 +1000

    updating ui

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -39,22 +39,6 @@ namespace Web.Controllers
             return View(new ApiXResponse { Code = response.StatusCode.ToString() });
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
